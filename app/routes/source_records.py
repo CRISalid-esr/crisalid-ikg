7a11e0d878b9404e97a3da1ed99de6f981a9203f
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-09-24 10:57:58 +0200

    Remove unused route

--- app/routes/source_records.py
+++ app/routes/source_records.py
@@ -8,7 +8,6 @@ from starlette.responses import JSONResponse
 
 from app.models.people import Person
 from app.models.source_records import SourceRecord
-from app.services.people.people_service import PeopleService
 from app.services.source_records.source_record_service import SourceRecordService
 
 router = APIRouter()
@@ -42,27 +41,6 @@ async def create_source_record(
     created_source_record = await source_record_service.create_source_record(
         source_record=source_record,
         harvested_for=person)
-    response_data = {"message": "Source record created successfully", "cource_record":
+    response_data = {"message": "Source record created successfully", "source_record":
         created_source_record}
     return JSONResponse(jsonable_encoder(response_data), status_code=status.HTTP_201_CREATED)
-
-
-@router.put(
-    "/",
-    name="update-person",
-)
-async def update_person(
-        people_service: Annotated[PeopleService, Depends(PeopleService)],
-        person: Person | None,
-) -> JSONResponse:
-    """
-    Updates a person
-
-    :param people_service:
-    :param person: entity built from fields
-    :return: json response
-    """
-    updated_person = await people_service.update_person(person)
-    response_data = {"message": f"Person with id {person.id} updated successfully",
-                     "person": updated_person}
-    return JSONResponse(jsonable_encoder(response_data), status_code=status.HTTP_200_OK)
