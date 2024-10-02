fa06537c8af9291d1941f01ce953fdc7bcc604b0
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-02 07:23:33 +0200

    Fix issue in source_records tests

--- tests/fixtures/source_record_fixtures.py
+++ tests/fixtures/source_record_fixtures.py
@@ -15,6 +15,15 @@ async def fixture_scanr_thesis_source_record_pydantic_model(_base_path) -> Perso
         _source_record_json_data_from_file(_base_path, "scanr_thesis_source_record"))
 
 
+@pytest_asyncio.fixture(name="scanr_thesis_source_record_json_data")
+async def fixture_scanr_thesis_source_record_json_data(_base_path) -> dict:
+    """
+    Create a thesis source record dict from ScanR data
+    :return: basic source record dict from ScanR data
+    """
+    return _source_record_json_data_from_file(_base_path, "scanr_thesis_source_record")
+
+
 @pytest_asyncio.fixture(name="idref_thesis_source_record_pydantic_model")
 async def fixture_idref_thesis_source_record_pydantic_model(_base_path) -> Person:
     """
@@ -24,14 +33,6 @@ async def fixture_idref_thesis_source_record_pydantic_model(_base_path) -> Perso
     return _source_record_from_json_data(
         _source_record_json_data_from_file(_base_path, "idref_thesis_source_record"))
 
-
-@pytest_asyncio.fixture(name="scanr_thesis_source_record_json_data")
-async def fixture_scanr_thesis_source_record_json_data(_base_path) -> dict:
-    """
-    Create a thesis source record dict from ScanR data
-    :return: basic source record dict from ScanR data
-    """
-    return _source_record_json_data_from_file(_base_path, "scanr_thesis_source_record")
 
 @pytest_asyncio.fixture(name="idref_thesis_source_record_json_data")
 async def fixture_idref_thesis_source_record_json_data(_base_path) -> dict:
