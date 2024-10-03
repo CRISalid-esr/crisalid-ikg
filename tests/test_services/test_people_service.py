228a511ee75c4af96e81e31e053b0251b3731145
L-Bara <62282795+L-Bara@users.noreply.github.com>
2024-10-03 16:46:07 +0200

    Add test for people services (#44)

---
+++ tests/test_services/test_people_service.py
@@ -0,0 +1,33 @@
+from app.models.people import Person
+from app.services.people.people_service import PeopleService
+
+
+async def test_create_person(
+        person_pydantic_model: Person,
+        persisted_research_structure_pydantic_model  # pylint: disable=unused-argument
+) -> None:
+    """
+    Given a basic person pydantic model
+    When the person is added to the graph
+    Then the person can be read from the graph
+    :param person_pydantic_model:
+    :return:
+    """
+    service = PeopleService()
+    await service.create_person(person_pydantic_model)
+    fetched_person = await service.get_person(person_pydantic_model.id)
+    assert fetched_person.id == person_pydantic_model.id
+    assert len(fetched_person.identifiers) == len(person_pydantic_model.identifiers)
+    for identifier in person_pydantic_model.identifiers:
+        assert any(
+            fetched_identifier.type == identifier.type
+            and fetched_identifier.value == identifier.value
+            for fetched_identifier in fetched_person.identifiers
+        )
+    assert len(fetched_person.names) == len(person_pydantic_model.names)
+    assert len(person_pydantic_model.memberships) == 1
+    assert len(fetched_person.memberships) == len(person_pydantic_model.memberships)
+    assert len(person_pydantic_model.memberships[0].research_structure.identifiers) == 1
+    assert len(fetched_person.memberships[0].research_structure.identifiers) == len(
+        person_pydantic_model.memberships[0].research_structure.identifiers
+    )
