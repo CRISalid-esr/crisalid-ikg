afa4c25fcd8bb35a2db6b57e76a0be3e6f9905c9
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-10 08:36:42 +0200

    Add subjects to source record in the graph

--- app/graph/neo4j/queries/create_source_record.cypher
+++ app/graph/neo4j/queries/create_source_record.cypher
@@ -1,19 +1,21 @@
 CREATE (s:SourceRecord {uid: $source_record_uid, harvester: $harvester, source_identifier: $source_identifier})
 WITH s
 FOREACH (title IN $titles |
-    CREATE (t:Literal {value: title.value, language: title.language})
-    CREATE (s)-[:HAS_TITLE]->(t)
+  CREATE (t:Literal {value: title.value, language: title.language})
+  CREATE (s)-[:HAS_TITLE]->(t)
 )
-WITH s
 FOREACH (abstract IN $abstracts |
-    CREATE (a:Literal {value: abstract.value, language: abstract.language})
-    CREATE (s)-[:HAS_ABSTRACT]->(a)
+  CREATE (a:Literal {value: abstract.value, language: abstract.language})
+  CREATE (s)-[:HAS_ABSTRACT]->(a)
 )
-WITH s
 FOREACH (identifier IN $identifiers |
-    CREATE (i:PublicationIdentifier {type: identifier.type, value: identifier.value})
-    CREATE (s)-[:HAS_IDENTIFIER]->(i)
+  MERGE (i:PublicationIdentifier {type: identifier.type, value: identifier.value})
+  MERGE (s)-[:HAS_IDENTIFIER]->(i)
 )
 WITH s, $person_uid AS person_uid
 MATCH (p:Person {uid: person_uid})
 MERGE (s)-[:HARVESTED_FOR]->(p)
+WITH s, $subject_uris AS subject_uris
+UNWIND subject_uris AS subject_uri
+MATCH (sub:Concept {uri: subject_uri})
+MERGE (s)-[:HAS_SUBJECT]->(sub)
--- app/services/source_records/source_record_service.py
+++ app/services/source_records/source_record_service.py
@@ -1,10 +1,15 @@
+from loguru import logger
+from neo4j.exceptions import Neo4jError
+
 from app.config import get_app_settings
+from app.errors.reference_owner_not_found_error import ReferenceOwnerNotFoundError
 from app.graph.generic.abstract_dao_factory import AbstractDAOFactory
 from app.graph.generic.dao_factory import DAOFactory
 from app.graph.neo4j.person_dao import PersonDAO
 from app.graph.neo4j.source_record_dao import SourceRecordDAO
 from app.models.people import Person
 from app.models.source_records import SourceRecord
+from app.services.concepts.concept_service import ConceptService
 from app.signals import source_record_created
 
 
@@ -27,8 +32,20 @@ class SourceRecordService:
         people_dao: PersonDAO = factory.get_dao(Person)
         person = await people_dao.find(harvested_for)
         if not person:
-            raise ValueError(f"Person with uid {harvested_for.uid} does not exist")
+            raise ReferenceOwnerNotFoundError(f"Person with uid {harvested_for.uid} does not exist")
         source_record_dao: SourceRecordDAO = factory.get_dao(SourceRecord)
+        concept_service = ConceptService()
+        registered_concepts = []
+        for subject in source_record.subjects:
+            try:
+                concept = await concept_service.create_or_update_concept(subject)
+                registered_concepts.append(concept)
+            except ValueError as e:
+                logger.error(f"Invalid data error while creating or updating concept {subject} :"
+                             f" {e}")
+            except Neo4jError as e:
+                logger.error(f"Database error while creating or updating concept {subject} : {e}")
+        source_record.subjects = registered_concepts
         record_id, status, _ = await source_record_dao.create(source_record=source_record,
                                                               harvested_for=person)
         if status is SourceRecordDAO.Status.CREATED:
