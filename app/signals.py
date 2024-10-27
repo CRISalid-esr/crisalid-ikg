0cfa392795b40bcbf566a7df4da99001125b5fa3
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2024-10-27 09:07:23 +0100

    Fetch publications for unchanged people (#110)

--- app/signals.py
+++ app/signals.py
@@ -1,6 +1,7 @@
 from blinker import signal
 
 person_created = signal('person-created')
+person_unchanged = signal('person-unchanged')
 person_identifiers_updated = signal('person-identifiers-updated')
 person_deleted = signal('person-deleted')
 
