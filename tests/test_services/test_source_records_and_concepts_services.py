b44e869bdf79d3409f0120e461510f004f4ddfd8
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-31 17:24:14 +0100

    Restore tests

--- tests/test_services/test_source_records_and_concepts_services.py
+++ tests/test_services/test_source_records_and_concepts_services.py
@@ -64,7 +64,8 @@ async def test_create_two_source_records_with_common_concepts_and_different_alt_
 ) -> None:
     """
     Given two persisted person pydantic_model,
-      And a source record for person b with a concept
+    When a source record for person a with concepts is created
+    And a source record for person b with a concept
     in common but with different alt_labels is created
     Then the records should share the same concept with all the alt_labels referenced.
 
@@ -107,11 +108,21 @@ async def test_create_two_source_records_with_common_concepts_and_different_alt_
     assert all(
         alt_label in fetched_concept.alt_labels for alt_label in alt_labels_to_check
     )
+
+    await record_service.create_source_record(
+        source_record=scanr_record_with_person_b_as_contributor_pydantic_model,
+        harvested_for=persisted_person_b_pydantic_model)
+    fetched_scanr_source_record_for_person_b = await record_service.get_source_record(
+        scanr_record_with_person_b_as_contributor_pydantic_model.uid)
+    assert (
+            fetched_scanr_source_record_for_person_b.uid ==
+            scanr_record_with_person_b_as_contributor_pydantic_model.uid
+    )
+
     subject = next(subject for subject in
                    scanr_record_with_person_b_as_contributor_pydantic_model.subjects
                    if subject.uri == concept_uri
                    )
-
 
     alt_labels_to_check.extend(
         alt_label
