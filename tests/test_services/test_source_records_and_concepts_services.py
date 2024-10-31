70d95967d108b06d17d0ea725534ec445712c81f
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-10-31 17:20:37 +0100

    Fix merge errors

--- tests/test_services/test_source_records_and_concepts_services.py
+++ tests/test_services/test_source_records_and_concepts_services.py
@@ -55,10 +55,15 @@ async def test_create_two_source_records_with_same_concepts(
     assert any(updated_pref_label == pref_label.value for pref_label in
                fetched_concept.pref_labels)
 
+
 async def test_create_two_source_records_with_common_concepts_and_different_alt_labels(
         persisted_person_a_pydantic_model: Person,
         scanr_record_with_person_a_as_contrib_and_additional_alt_labels_pyd_model: SourceRecord,
         persisted_person_b_pydantic_model: Person,
+        scanr_record_with_person_b_as_contributor_pydantic_model: SourceRecord
+) -> None:
+    """
+    Given two persisted person pydantic_model,
       And a source record for person b with a concept
     in common but with different alt_labels is created
     Then the records should share the same concept with all the alt_labels referenced.
@@ -102,10 +107,11 @@ async def test_create_two_source_records_with_common_concepts_and_different_alt_
     assert all(
         alt_label in fetched_concept.alt_labels for alt_label in alt_labels_to_check
     )
-        subject = next(subject for subject in
+    subject = next(subject for subject in
                    scanr_record_with_person_b_as_contributor_pydantic_model.subjects
                    if subject.uri == concept_uri
                    )
+
 
     alt_labels_to_check.extend(
         alt_label
