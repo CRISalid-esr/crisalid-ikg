2213c6a6bee94264e0f9a285b25693906482ab1a
L-Bara <e-bara@outlook.com>
2024-11-27 13:46:52 +0100

    Add test case when 3 source records are harvested and one have common identifiers with the 2 others

--- tests/test_services/test_source_records_and_equivalence_service.py
+++ tests/test_services/test_source_records_and_equivalence_service.py
@@ -48,7 +48,7 @@ async def test_create_multiple_source_records_with_common_id_for_multiple_person
 
     for record in [open_alex_fetched_source_record, scanr_fetched_source_record,
                    hal_fetched_source_record]:
-        assert open_alex_fetched_source_record
+        assert record
         assert any(
             fetched_identifier.type == PublicationIdentifierType.HAL
             and fetched_identifier.value == "hal-00000000"
@@ -65,4 +65,41 @@ async def test_create_multiple_source_records_with_common_id_for_multiple_person
         scanr_article_a_v2_source_record_pydantic_model.uid,
         hal_article_a_source_record_pydantic_model.uid,
         open_alex_article_a_source_record_pydantic_model.uid
+    ])
+
+
+async def test_create_source_records_with_one_having_common_id_with_others(
+        persisted_person_a_pydantic_model: Person,
+        scanr_article_a_source_record_pydantic_model: SourceRecord,
+        open_alex_article_b_source_record_pydantic_model: SourceRecord,
+        idref_article_a_source_record_pydantic_model: SourceRecord,
+) -> None:
+    """
+    Given 3 source records harvested, with one of them having common identifiers with the other two,
+    When the source records are added to the graph
+    Then the source records are added, and they are related to each other with a relationship.
+    """
+    source_record_service = SourceRecordService()
+
+    for record in [
+        scanr_article_a_source_record_pydantic_model,
+        open_alex_article_b_source_record_pydantic_model,
+        idref_article_a_source_record_pydantic_model
+    ]:
+        await source_record_service.create_source_record(
+            source_record=record,
+            harvested_for=persisted_person_a_pydantic_model)
+
+        assert await source_record_service.get_source_record(record.uid)
+
+    factory = AbstractDAOFactory().get_dao_factory("neo4j")
+    document_dao: DocumentDAO = cast(DocumentDAO, factory.get_dao(Document))
+    document = await document_dao.get_textual_document_by_source_record_uid(
+        scanr_article_a_source_record_pydantic_model.uid)
+    assert document is not None
+    assert document.to_be_recomputed is True
+    assert sorted(document.source_record_uids) == sorted([
+        scanr_article_a_source_record_pydantic_model.uid,
+        open_alex_article_b_source_record_pydantic_model.uid,
+        idref_article_a_source_record_pydantic_model.uid
     ])
