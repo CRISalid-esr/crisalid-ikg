a18a9e03fae76e3669fb4484de245d939277d0af
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-12-20 11:36:10 +0100

    Handle transient errors

--- app/errors/database_error.py
+++ app/errors/database_error.py
@@ -1,4 +1,5 @@
 """ Database error handler. """
+import asyncio
 from functools import wraps
 
 from neo4j.exceptions import (
@@ -10,6 +11,9 @@ from neo4j.exceptions import (
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
+
+MAX_RETRIES = 3
+RETRY_DELAY = 0.5
 
 
 class DatabaseError(Exception):
@@ -40,23 +44,37 @@ async def database_error_handler(
 def handle_database_errors(func):
     """
     Decorator to handle various Neo4j exceptions by converting them to a custom DatabaseError.
+    Includes retry logic for TransientError with deadlock detection.
     """
 
     @wraps(func)
     async def wrapper(*args, **kwargs):
-        try:
-            return await func(*args, **kwargs)
-        except Neo4jDatabaseError as e:
-            raise DatabaseError("A database error occurred.") from e
-        except ServiceUnavailable as e:
-            raise DatabaseError("The Neo4j service is unavailable or misconfigured.") from e
-        except TransientError as e:
-            raise DatabaseError("A transient error occurred.") from e
-        except ClientError as e:
-            raise DatabaseError(
-                "A client error occurred, possibly due to an invalid query or constraint.") from e
-        except DriverError as e:
-            raise DatabaseError(
-                "A driver or session error occurred, possibly due to configuration issues.") from e
+        retries = 0
+        while retries <= MAX_RETRIES:
+            try:
+                return await func(*args, **kwargs)
+            except TransientError as e:
+                if e.code == 'Neo.TransientError.Transaction.DeadlockDetected':
+                    if retries < MAX_RETRIES:
+                        retries += 1
+                        await asyncio.sleep(RETRY_DELAY)
+                        continue
+                    raise DatabaseError(
+                        f"Max retries reached for deadlock detected: {e.code}"
+                    ) from e
+                raise DatabaseError("A transient error occurred.") from e
+            except Neo4jDatabaseError as e:
+                raise DatabaseError("A database error occurred.") from e
+            except ServiceUnavailable as e:
+                raise DatabaseError("The Neo4j service is unavailable or misconfigured."
+                                    ) from e
+            except ClientError as e:
+                raise DatabaseError(
+                    "A client error occurred, possibly due to an invalid query or constraint."
+                ) from e
+            except DriverError as e:
+                raise DatabaseError(
+                    "A driver or session error occurred, possibly due to configuration issues."
+                ) from e
 
     return wrapper
