95551ff242912514f1e67c7c51f96cdf5c3643b8
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-12-20 04:05:24 +0100

    Remove unused uid

--- app/models/source_organization_identifiers.py
+++ app/models/source_organization_identifiers.py
@@ -1,25 +1,10 @@
-from typing import ClassVar
-
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel
 
 
 class SourceOrganizationIdentifier(BaseModel):
     """
     Source Organization Identifier API model
     """
-    IDENTIFIER_SEPARATOR: ClassVar[str] = "-"
 
     type: str
     value: str
-
-    @model_validator(mode="before")
-    @classmethod
-    def _build_uid(cls, values):
-        type_ = values.get("type")
-        value = values.get("value")
-        if not type_ or not value:
-            return values
-        values["uid"] = (f"{str(type_).lower()}"
-                         f"{SourceOrganizationIdentifier.IDENTIFIER_SEPARATOR}"
-                         f"{value}")
-        return values
