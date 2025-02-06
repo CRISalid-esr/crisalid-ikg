dd1acf8fceb1a401a302a66eac20fa470ab8faac
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-02-06 10:57:42 +0100

    Compute partial Iso8601 dates as intervals

--- app/models/document.py
+++ app/models/document.py
@@ -1,4 +1,5 @@
 import uuid
+from datetime import datetime
 from typing import List, Optional
 
 from pydantic import BaseModel, model_validator
@@ -6,6 +7,7 @@ from pydantic import BaseModel, model_validator
 from app.models.concepts import Concept
 from app.models.contributions import Contribution
 from app.models.literal import Literal
+from app.utils.date.partial_iso_8601 import partial_iso8601_interval
 
 
 class Document(BaseModel):
@@ -21,8 +23,11 @@ class Document(BaseModel):
     to_be_deleted: bool = False
     to_be_merged_into_uid: Optional[str] = None
     source_record_uids: Optional[List[str]] = None
-
     contributions: Optional[List[Contribution]] = []
+
+    publicationDate: Optional[str] = None
+    publicationDateStart: Optional[datetime] = None
+    publicationDateEnd: Optional[datetime] = None
 
     @model_validator(mode="before")
     @classmethod
@@ -32,4 +37,17 @@ class Document(BaseModel):
         """
         if "uid" not in values or not values["uid"]:
             values["uid"] = str(uuid.uuid4())
+        return values
+
+    @model_validator(mode="before")
+    @classmethod
+    def _compute_publication_dates(cls, values):
+        """
+        Compute publicationDateStart and publicationDateEnd from publicationDate
+        """
+        publication_date = values.get("publicationDate")
+        if publication_date:
+            start, end = partial_iso8601_interval(publication_date)
+            values["publicationDateStart"] = start
+            values["publicationDateEnd"] = end
         return values
--- app/utils/date/partial_iso_8601.py
+++ app/utils/date/partial_iso_8601.py
@@ -1,6 +1,7 @@
+import calendar
 import re
 from datetime import datetime
-from typing import Optional
+from typing import Optional, Tuple
 
 from loguru import logger
 
@@ -40,3 +41,35 @@ def parse_partial_iso8601(date_str: str) -> Optional[str]:
             return None
 
     return None
+
+
+def partial_iso8601_interval(date: str) -> Tuple[Optional[datetime], Optional[datetime]]:
+    """
+    Parses a partial ISO 8601 date string and returns a start and end date.
+    :param date: a string representing a date in one of the following formats:
+    :return: a tuple of two datetime objects representing the start and end date of the interval
+    """
+    parsed_date = parse_partial_iso8601(date)
+    if not parsed_date:
+        return None, None
+
+    try:
+        if len(parsed_date) == 4:  # YYYY
+            start_date = datetime(int(parsed_date), 1, 1)
+            end_date = datetime(int(parsed_date), 12, 31, 23, 59, 59)
+        elif len(parsed_date) == 7:  # YYYY-MM
+            year, month = map(int, parsed_date.split("-"))
+            start_date = datetime(year, month, 1)
+            last_day = calendar.monthrange(year, month)[1]
+            end_date = datetime(year, month, last_day, 23, 59, 59)
+
+        elif len(parsed_date) == 10:  # YYYY-MM-DD
+            start_date = datetime.strptime(parsed_date, "%Y-%m-%d")
+            end_date = start_date.replace(hour=23, minute=59, second=59)
+        else:
+            return None, None
+
+        return start_date, end_date
+    except ValueError:
+        logger.error(f"Invalid parsed date: {parsed_date}")
+        return None, None
