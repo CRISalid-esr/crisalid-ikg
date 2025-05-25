b103a46aee06c43509dd3086692c4308a61c9f2d
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-05-25 09:09:28 +0200

    Update identifier names to match new specification

--- app/models/identifier_types.py
+++ app/models/identifier_types.py
@@ -52,13 +52,15 @@ PERSON_IDENTIFIER_PATTERNS = {
 class OrganizationIdentifierType(AgentIdentifierType):
     """Organization identifier types"""
     IDREF = "IdRef"
-    ROR = "ROR"
-    RNSR = "RNSR"
+    ROR = "ror"
+    RNSR = "nns"
     LOCAL = "local"
     UAI = "UAI"
     SIREN = "SIREN"
     SIRET = "SIRET"
     WIKIDATA = "Wikidata"
+    SCOPUS_ID = "scopus_id"
+    HAL= "hal"
 
 
 class PublicationIdentifierType(Enum):
