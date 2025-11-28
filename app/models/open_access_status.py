f1a1065f103826ff0d8f71f9592bda4f44d5f9fd
eblettery <emile.blettery@gmail.com>
2025-11-28 17:47:37 +0100

    Add enums for OA statuses

--- app/models/open_access_status.py
+++ app/models/open_access_status.py
@@ -1,8 +1,29 @@
 import datetime
+from enum import Enum
 from typing import Optional
 
 from pydantic import BaseModel
 
+
+class OAStatus(str, Enum):
+    """
+    Enumeration of generic OA statuses
+    """
+    GREEN = "green"
+    CLOSED = "closed"
+
+
+class UnpaywallOAStatus(str, Enum):
+    """
+    Enumeration of Unpaywall OA statuses
+    """
+    GREEN = "green"
+    DIAMOND = "diamond"
+    GOLD = "gold"
+    BRONZE = "bronze"
+    HYBRID = "hybrid"
+    OTHER = "other"
+    CLOSED = "closed"
 
 class OpenAccessStatus(BaseModel):
     """
@@ -12,6 +33,6 @@ class OpenAccessStatus(BaseModel):
     oa_computed_status: Optional[bool] = False
     oa_upw_success_status: Optional[bool] = None
     oa_doaj_success_status: Optional[bool] = None
-    oa_status: Optional[str] = None
-    upw_oa_status: Optional[str] = None
+    oa_status: Optional[OAStatus] = None
+    upw_oa_status: Optional[UnpaywallOAStatus] = None
     coar_oa_status: Optional[str] = None
