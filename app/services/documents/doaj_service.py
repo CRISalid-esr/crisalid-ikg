77038c6fa2ee762a17aff53d31136978277cc385
eblettery <emile.blettery@gmail.com>
2025-11-28 17:46:35 +0100

    Add dataclasses to api services

--- app/services/documents/doaj_service.py
+++ app/services/documents/doaj_service.py
@@ -1,7 +1,10 @@
-from app.utils.publication_api.publication_api_service import PublicationApiService
+from dataclasses import dataclass
+from typing import Optional
+
+from app.utils.api.api_service import ApiService
 
 
-class DoajService(PublicationApiService):
+class DoajService(ApiService):
     """
     Service to check APC Status in DOAJ
     """
@@ -14,24 +17,29 @@ class DoajService(PublicationApiService):
         url = f"{self.base_url}{issn}"
         return await self._fetch_json(url)
 
-    async def get_apc_status(self, issn: str) -> dict:
+    @dataclass
+    class DoajResponseSchema:
+        """
+        Dataclass for information gathered from DOAJ
+        """
+        doaj_success: Optional[bool] = None
+        has_apc: Optional[bool] = None
+
+    async def get_apc_status(self, issn: str) -> DoajResponseSchema:
         """
         Get data from DOAJ API to check if a journal has APC
         """
-        apc_data = {
-            "doaj_success": None,
-            "has_apc": None,
-        }
+        apc_data = self.DoajResponseSchema()
 
         json_data = await self._fetch_doaj_json(issn)
         if json_data is None:
-            apc_data["doaj_success"] = False
+            apc_data.doaj_success = False
             return apc_data
 
-        apc_data["doaj_success"] = True
+        apc_data.doaj_success = True
 
         if json_data.get("results"):
-            apc_data["has_apc"] = json_data.get("results")[0].get(
+            apc_data.has_apc = json_data.get("results")[0].get(
                 "bibjson", {}).get("apc", {}).get("has_apc", None)
 
         return apc_data
--- app/services/documents/unpaywall_service.py
+++ app/services/documents/unpaywall_service.py
@@ -1,8 +1,13 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional
+
+from app.models.open_access_status import UnpaywallOAStatus
 from app.services.documents.doaj_service import DoajService
-from app.utils.publication_api.publication_api_service import PublicationApiService
+from app.utils.api.api_service import ApiService
 
 
-class UnpaywallService(PublicationApiService):
+class UnpaywallService(ApiService):
     """
     Service to check Open Access Status in Unpaywall
     """
@@ -14,36 +19,45 @@ class UnpaywallService(PublicationApiService):
         url = f"{self.base_url}/{doi}?email={self.settings.email_unpaywall}"
         return await self._fetch_json(url)
 
-    async def get_data(self, doi: str) -> dict:
+    @dataclass
+    class UpwResponseSchema:
+        """
+        Dataclass for information gathered from Unpaywall
+        """
+        upw_success: Optional[bool] = None
+        upw_status: Optional[str] = None
+        repository_location: Optional[bool] = None
+        doaj_success: Optional[bool] = None
+        embargo_date: Optional[datetime] = None
+
+    async def get_data(self, doi: str) -> UpwResponseSchema:
         """
         Get data from Unpaywall API to be used to compute OpenAccess status
         """
-        oa_data = {
-            "upw_success": None,
-            "upw_status": None,
-            "repository_location": None,
-            "doaj_success": None,
-            "embargo_date": None
-        }
+        oa_data = self.UpwResponseSchema()
 
         json_data = await self._fetch_unpaywall_json(doi)
         if json_data is None:
-            oa_data["upw_success"] = False
+            # The error may result from either an invalid doi or from platform unavailability
+            oa_data.upw_success = False
             return oa_data
 
-        oa_data["upw_success"] = True
-        oa_data["upw_status"] = json_data.get("oa_status", None)
+        assert isinstance(json_data, dict), "Unpaywall API response should be a json dict"
 
-        oa_data["repository_location"] = None if not json_data.get("oa_locations") else any(
+        oa_data.upw_success = True
+        oa_data.upw_status = json_data.get("oa_status", None)
+
+        oa_data.repository_location = None if not json_data.get("oa_locations") else any(
             loc.get("host_type") == "repository" for loc in json_data["oa_locations"]
         )
 
         # Missing potential embargo date to get when example arises
 
-        if oa_data["upw_status"] == "Gold" and json_data.get("journal_is_in_doaj", False):
+        if (oa_data.upw_status and oa_data.upw_status.lower() == UnpaywallOAStatus.GOLD
+                and json_data.get("journal_is_in_doaj", False)):
             apc_data = await DoajService().get_apc_status(json_data.get("journal_issn_l"))
-            oa_data["doaj_success"] = apc_data["doaj_success"]
-            if not apc_data["has_apc"]:
-                oa_data["upw_status"] = "Diamond"
+            oa_data.doaj_success = apc_data.doaj_success
+            if not apc_data.has_apc:
+                oa_data.upw_status = "diamond"
 
         return oa_data
