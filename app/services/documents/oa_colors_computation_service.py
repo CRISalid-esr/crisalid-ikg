723834fb7dde25e9a47fc1dd6d49050a60d9635f
eblettery <emile.blettery@gmail.com>
2025-11-28 17:48:46 +0100

    Refactor with dataclasses and Enums

--- app/services/documents/oa_colors_computation_service.py
+++ app/services/documents/oa_colors_computation_service.py
@@ -4,10 +4,9 @@ from typing import List
 from app.config import get_app_settings
 from app.models.document import Document
 from app.models.identifier_types import PublicationIdentifierType
-from app.models.open_access_status import OpenAccessStatus
+from app.models.open_access_status import OpenAccessStatus, UnpaywallOAStatus, OAStatus
 from app.models.source_records import SourceRecord
 from app.services.documents.unpaywall_service import UnpaywallService
-
 
 class OAColorsComputationService:
     """
@@ -37,12 +36,12 @@ class OAColorsComputationService:
         document_oa_status = self.document.open_access_status
         document_oa_status.oa_computation_timestamp = datetime.now()
         document_oa_status.oa_status = next(
-            ('Green' for sr in self.source_records if sr.harvester in ['HAL','Hal','hal']
+            (OAStatus.GREEN for sr in self.source_records if sr.harvester.lower() == 'hal'
                   and getattr(sr.custom_metadata, 'hal_submit_type', None) == 'file'),
             None)
 
         if doi is None:
-            if document_oa_status.oa_status == 'Green':
+            if document_oa_status.oa_status == OAStatus.GREEN:
                 document_oa_status.oa_computed_status = True
                 return self.document
 
@@ -50,19 +49,26 @@ class OAColorsComputationService:
 
         upw_data = await UnpaywallService().get_data(doi)
 
-        document_oa_status.upw_oa_status = upw_data['upw_status']
-        document_oa_status.oa_upw_success_status = upw_data['upw_success']
-        document_oa_status.oa_doaj_success_status = upw_data['doaj_success']
+        document_oa_status.upw_oa_status = UnpaywallOAStatus(upw_data.upw_status) \
+            if upw_data.upw_status else None
+        document_oa_status.oa_upw_success_status = upw_data.upw_success
+        document_oa_status.oa_doaj_success_status = upw_data.doaj_success
         if document_oa_status.oa_upw_success_status:
             document_oa_status.oa_computed_status = True
 
-        if document_oa_status.oa_status != 'Green':
-            if upw_data["repository_location"]:
+        if document_oa_status.oa_status != OAStatus.GREEN:
+            if upw_data.repository_location:
                 document_oa_status.oa_computed_status = True
-                document_oa_status.oa_status = 'Green'
-            else:
+                document_oa_status.oa_status = OAStatus.GREEN
+            elif upw_data.upw_status and upw_data.upw_status.lower() == OAStatus.GREEN:
                 document_oa_status.oa_computed_status = True
-                document_oa_status.oa_status = 'Closed'
+                document_oa_status.oa_status = OAStatus.GREEN
+            elif upw_data.upw_status and upw_data.upw_status.lower() == OAStatus.CLOSED:
+                document_oa_status.oa_computed_status = True
+                document_oa_status.oa_status = OAStatus.CLOSED
+            elif upw_data.upw_status is None:
+                document_oa_status.oa_computed_status = True
+                document_oa_status.oa_status = OAStatus.CLOSED
 
         # Add COAR color based on info from upw_data (when embargo date available...)
 
