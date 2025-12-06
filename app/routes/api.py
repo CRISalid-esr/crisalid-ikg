a56d2d299c83e627cc01691c49f73d85761941f5
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-12-06 16:11:35 +0100

    Enable healthness route

--- app/routes/api.py
+++ app/routes/api.py
@@ -4,9 +4,10 @@ API redirection router
 
 from fastapi import APIRouter
 
-from app.routes import people, organizations, source_records
+from app.routes import people, organizations, source_records, healthness
 
 router = APIRouter()
 router.include_router(people.router, tags=["people"], prefix="/person")
 router.include_router(organizations.router, tags=["organizations"], prefix="/organization")
 router.include_router(source_records.router, tags=["source_records"], prefix="/source_records")
+router.include_router(healthness.router, tags=["healthness"], prefix="/healthness")
