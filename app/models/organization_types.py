2d40b5c42d555fc233500fa1dd94bce676f21704
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2026-07-18 18:51:18 +0200

    fix: allow GIS, GIP, LABEX, UR national types for institution subdivisions

--- app/models/organization_types.py
+++ app/models/organization_types.py
@@ -26,6 +26,9 @@ class NationalOrganizationType(str, Enum):
     FAC = "FAC"
     FDR = "FDR"
     ED = "ED"
+    GIS = "GIS"
+    GIP = "GIP"
+    LABEX = "LABEX"
     TEAM = "TEAM"
     THEME = "THEME"
 
@@ -63,6 +66,10 @@ ALLOWED_NATIONAL_TYPES_BY_GENERIC_TYPE: dict = {
         NationalOrganizationType.UFR,
         NationalOrganizationType.FAC,
         NationalOrganizationType.FDR,
+        NationalOrganizationType.GIS,
+        NationalOrganizationType.GIP,
+        NationalOrganizationType.LABEX,
+        NationalOrganizationType.UR,
     },
     GenericOrganizationType.DOCTORAL_SCHOOL: {
         NationalOrganizationType.ED,
